c4bc5a6279c05583b660295660c0d600d50e3043
Ayman-AlAmin <e2291719@cmaisonneuve.qc.ca>
2025-12-05 14:09:13 -0500

    Etape 1: Connection au Wifi avec gestion d'erreur

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,62 @@
 #include <Arduino.h>
+#include <WiFi.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Information de connexion WiFi
+const char* ssid = "Ayman";
+const char* password = "12345678"; // PAS SUR A REVOIR!!!!!!!!!!!!!!
+
+// print interval
+unsigned long lastIpPrint = 0;
+//const unsigned long ipPrintInterval = 5000; // 5 seconds
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  delay(1000);
+  
+  // Connect to WiFi
+  Serial.println();
+  Serial.print("Connexion au WiFi: ");
+  Serial.println(ssid);
+  
+  // Début de la connexion au WiFi de l'école
+  WiFi.begin(ssid, password);
+
+  // Attente de connection avec timeout simple
+  unsigned long startAttemptTime = millis();
+  const unsigned long timeoutMs = 15000; // 15 seconds max
+
+  while (WiFi.status() != WL_CONNECTED && (millis() - startAttemptTime) < timeoutMs) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  // Affichage de l'IP quand connexion réussite 
+  Serial.println();
+
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Erreur: impossible de se connecter au WiFi. Verifiez le reseau ou le mot de passe.");
+    return;
+  }
+
+  // Affichage de l'IP quand connexion réussite 
+  Serial.println("WiFi connected!");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  lastIpPrint = millis();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  unsigned long now = millis();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (now - lastIpPrint >= 5000) {  
+    if (WiFi.status() == WL_CONNECTED) {
+      Serial.print("IP address: ");
+      Serial.println(WiFi.localIP());
+    } else {
+      Serial.println("WiFi not connected");
+    }
+    lastIpPrint = now;
+  }
+
+  delay(100);
 }
